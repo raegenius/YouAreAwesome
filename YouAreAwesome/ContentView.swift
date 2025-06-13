ad8f031b26362deaf8f5a43aca17d8fc9c1559d1
Raemur <raemur@gmail.com>
2025-06-12 21:14:08 -0700

    Make images and messages shown random.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -34,21 +34,10 @@ struct ContentView: View {
             Spacer()
             
             Button("Show Messsage") {
-                var messages : [String] = ["You are Awesome!", "You are Great!"]
-                messages += ["You are Amazing", "You Rock", "God's got you", "Exceptional is your middle name!"]
-                imageNumber += 1
-                
-                if imageNumber == 9 {
-                    imageNumber = 0
-                    
-                }
-                messageNumber += 1
-                if messageNumber == messages.count {
-                    messageNumber = 0
-                    
-                }
-                message = messages[messageNumber]
-                imageName = "image" + String(imageNumber)
+                let messages : [String] = ["You are Awesome!", "You are Great!","You are Amazing", "You Rock", "God's got you", "Exceptional is your middle name!"]
+
+                message = messages[Int.random(in: 0...messages.count-1 )]
+                imageName = "image" + String(Int.random(in: 0...9))
                 
             }
             .buttonStyle(.borderedProminent)
